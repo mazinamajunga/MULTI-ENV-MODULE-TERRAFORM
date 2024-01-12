50a804b0485ff02256123384d7a9df927f058aad
patrick <patrickstephane@gmail.com>
2024-01-12 05:54:18 -0600

    update the readme

--- DEV_ENV/providers.tf
+++ DEV_ENV/providers.tf
@@ -35,14 +35,6 @@ provider "aws" {
 
 
 
-# resource "aws_dynamodb_table" "state_lock_table" {
-#   name           = "my-state-table"
-#   billing_mode   = "PAY_PER_REQUEST"
-#   hash_key       = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+
 
 
